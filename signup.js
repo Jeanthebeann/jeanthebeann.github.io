0522d773e8b6efd37da30c06529cae6d30bdbbd6
Jeanthebeann <99011344+Jeanthebeann@users.noreply.github.com>
2025-07-30 00:32:19 +0900

    init

---
+++ signup.js
@@ -0,0 +1,12 @@
+function login() {
+    const username = document.getElementById('username').value.trim();
+    const error = document.getElementById('error');
+
+    // Example check: Only allow "Jeff" or "Alice"
+    if (username.toLowerCase() === "jiwoo" || username === "엄마" || username === "이지우" || username.toLowerCase() === "lee jiwoo") {
+        window.location.href = "confess.html"; // redirect to another page
+    } 
+    else {
+        window.location.href = "empty.html";
+    }
+}
